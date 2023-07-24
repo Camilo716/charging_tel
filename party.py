d18c2b7a90c3c0f2caa4a724c0f132feecbf3221
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-24 00:37:47 +0000

    Add tests to client_code format

--- party.py
+++ party.py
@@ -1,9 +1,9 @@
 from trytond.pool import PoolMeta
-from tryton.model import fields
+from trytond.model import fields
 
 class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
 
-    client_code = fields.char('Client code', required=False)
+    client_code = fields.Char('Client Code', required=False)
 
     
---
+++ tests/test_party_client_code.py
@@ -0,0 +1,45 @@
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
+from trytond.model.exceptions import ValidationError
+
+class Party_client_code_TestCase(ModuleTestCase):
+    "Test Party client_code"
+    module = 'charging_tel'
+        
+    @with_transaction
+    def test_invalid_entry_should_throw_error(self):
+        pool = Pool()
+        party = pool.get('party.party')
+
+        party, = party.create([{'name': 'TEL'}])
+
+        with self.assertRaises(ValidationError):
+            party.client_code = '123546'
+            party.save()
+
+        with self.assertRaises(ValidationError):
+            party.client_code = '3515ABC'
+            party.save()
+
+        with self.assertRaises(ValidationError):
+            party.client_code = '_535'
+            party.save()
+
+        with self.assertRaises(ValidationError):
+            party.client_code = 'A535.'
+            party.save()
+
+    @with_transaction()
+    def test_valid_entry_dont_throw_error(self):
+        pool = Pool()
+        Party = pool.get('party.party')
+
+        party, = Party.create([{'name': 'TELO'}])
+
+        party.client_code = 'AB123'
+        party.save()
+
+        party.client_code = 'CP535'
+        party.save()
+
+del ModuleTestCase
